101676f10d0f8e1f5ca87cd3191a27a246e82530
TAMIL SELVAN M <tamilp435@gmail.com>
2024-12-17 13:01:24 +0530

    gulp installed

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
 
         $insertdata['name'] = 'Test';
         $insertdata['email'] = 'user@demo.com';
-        $insertdata['password'] = 'password';
+        $insertdata['password'] = 'Sirmutha@111';
 
         $user->insert($insertdata);
     }
