9fa323f8ca789f0ca186b468814f7a47264cb388
TAMIL SELVAN M <tamilp435@gmail.com>
2024-12-26 16:15:39 +0530

    video dynamic updated

--- app/Controllers/Admission.php
+++ app/Controllers/Admission.php
@@ -3,18 +3,23 @@
 namespace App\Controllers;
 
 use App\Models\BannerModel;
+use App\Models\VideoBannerModel;
 
 class Admission extends BaseController
 {
     public function index()
     {
         $bannerModel = new BannerModel();
+        $videoBannerModel = new VideoBannerModel();
+
         $banner = $bannerModel->where('page', 'admission')->where('is_published', 1)->first();
+        $video = $videoBannerModel->where('page', 'admission')->where('is_published', 1)->first();
 
         $data = [
             'page_title' => 'Admission',
             'page_code' => 'admission',
-            'banner' => $banner
+            'banner' => $banner,
+            'video' => $video
         ];
 
         return view('header', $data) . view('admission', $data) . view('footer');
--- app/Views/header.php
+++ app/Views/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= esc($page_title ?? 'Sir Mutha') ?></title>
 
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('images/favicon.png') ?>">
     <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/splide.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/slick.css') ?>">
