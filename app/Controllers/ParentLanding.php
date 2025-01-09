0d7a5d9b4c98a516fc5244fc79965b117659826c
TAMIL SELVAN M <tamilp435@gmail.com>
2025-01-09 16:18:48 +0530

    parent landing updated

--- app/Controllers/ParentLanding.php
+++ app/Controllers/ParentLanding.php
@@ -3,18 +3,23 @@
 namespace App\Controllers;
 
 use App\Models\BannerModel;
+use App\Models\VideoBannerModel;
 
 class ParentLanding extends BaseController
 {
     public function index()
     {
         $bannerModel = new BannerModel();
+        $videoBannerModel = new VideoBannerModel();
+
         $banner = $bannerModel->where('page', 'parent-landing')->where('is_published', 1)->first();
+        $video = $videoBannerModel->where('page', 'parentLanding')->where('is_published', 1)->first();
 
         $data = [
             'page_title' => 'Parent Landing',
             'page_code' => 'parent-landing',
-            'banner' => $banner
+            'banner' => $banner,
+            'video' => $video
         ];
 
         return view('header', $data) . view('parent-landing', $data) . view('footer');
