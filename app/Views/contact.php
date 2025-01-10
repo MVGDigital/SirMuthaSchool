50ede23786a8fe0a4e9e6729b77f713d643a1782
TAMIL SELVAN M <tamilp435@gmail.com>
2025-01-10 14:31:47 +0530

    banner updated

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -1,6 +1,7 @@
 <main>
     <!-- Banner Section -->
     <section class="banner-section">
+        <?php if (!empty($banner)): ?>
         <img src="<?= base_url('uploads/banner_images/' . $banner['desktop_image']); ?>" class="img-fluid deskTopImg"
             alt="<?= esc($banner['title']); ?>">
         <img src="<?= base_url('uploads/banner_images/' . $banner['mobile_image']); ?>" class="img-fluid mobImg w100"
@@ -10,9 +11,9 @@
             <p>A unit of the madras seva sadan</p>
         </div>
         <div class="banner-subTitle">
-            <h2>Contact Us</h2>
+            <h2><?= esc($banner['title']); ?></h2>
         </div>
-
+        <?php endif; ?>
     </section>
     <!-- Banner Section -->
 
