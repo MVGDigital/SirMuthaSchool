280483e872a7ccb146d6b7ceedeb6a1b3479a08d
TAMIL SELVAN M <tamilp435@gmail.com>
2025-01-25 16:04:32 +0530

    announcements dynamic updated

--- app/Controllers/Announcement.php
+++ app/Controllers/Announcement.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\BannerModel;
+use App\Models\Announcement as AnnouncementModel;
 
 class Announcement extends BaseController
 {
@@ -10,11 +11,14 @@ class Announcement extends BaseController
     {
         $bannerModel = new BannerModel();
         $banners = $bannerModel->where('page', 'about')->where('is_published', 1)->orderBy('sort_order', 'ASC')->findAll();
+        $announcementModel = new AnnouncementModel();
+        $announcements = $announcementModel->getAnnouncements();
 
         $data = [
             'page_title' => 'Announcement',
             'page_code' => 'announcement',
-            'banners' => $banners
+            'banners' => $banners,
+            'announcements' => $announcements
         ];
 
         return view('header', $data) . view('announcement', $data) . view('footer');
