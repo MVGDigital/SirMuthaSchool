9b727271b6950afed7ede9b724d6680c5dc0dcb8
TAMIL SELVAN M <tamilp435@gmail.com>
2025-01-25 11:38:47 +0530

    beyond curriculum dynamic updated

--- app/Controllers/BeyondCurriculum.php
+++ app/Controllers/BeyondCurriculum.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\BannerModel;
+use App\Models\AnnualEvent;
 
 class BeyondCurriculum extends BaseController
 {
@@ -11,10 +12,14 @@ class BeyondCurriculum extends BaseController
         $bannerModel = new BannerModel();
         $banner = $bannerModel->where('page', 'beyond-curriculum')->where('is_published', 1)->first();
 
+        $eventModel = new AnnualEvent();
+        $eventsByYear = $eventModel->getEventsByYear();
+
         $data = [
             'page_title' => 'BeyondCurriculum',
             'page_code' => 'beyond-curriculum',
-            'banner' => $banner
+            'banner' => $banner,
+            'events_by_year' => $eventsByYear
         ];
 
         return view('header', $data) . view('beyond-curriculum', $data) . view('footer');
