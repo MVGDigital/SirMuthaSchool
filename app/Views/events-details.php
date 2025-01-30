68d7eb4d73ed43ebcf71a44298b7232a37157189
Vmj41 <velmurugan.j@mvgdigital.com>
2025-01-30 10:04:25 +0530

    event detail page banner image updated for mobile

--- app/Views/events-details.php
+++ app/Views/events-details.php
@@ -39,8 +39,9 @@
     <!-- Event Details -->
     <section class="event-details-sec">
         <div class="img-overwite-wave">
-            <img src="<?= base_url('uploads/desktop_images/'. $event['desktop_image']);?>" class="img-fluid w100"
+            <img src="<?= base_url('uploads/desktop_images/'. $event['desktop_image']);?>" class="img-fluid deskTopImg w100"
                 alt="<?= esc($event['event_name']) ?>">
+            <img src="<?= base_url('images/facilities/green-capus.jpg') ?>" class="img-fluid mobImg w100" alt="Sir Mutha event images">
         </div>
 
         <div class="container-space wave-yellow-overwireLightBg ptb-80-30">
