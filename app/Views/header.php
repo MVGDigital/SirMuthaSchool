f28628ae67e328ff1f9810270a23da6e84d07a31
Vmj41 <velmurugan.j@mvgdigital.com>
2025-01-30 13:01:47 +0530

    Large screen issues fixed

--- app/Views/header.php
+++ app/Views/header.php
@@ -265,7 +265,7 @@
                     </div>
 
                     <div class="mobBtns">
-                        <a class="contactBtn" href="https://themadrassevasadan.org/be_a_part" traget="_blank">Donate</a>
+                        <a class="contactBtn" href="https://themadrassevasadan.org/be_a_part" target="_blank">Donate</a>
                         <a class="contactBtn" href="<?= base_url('contact') ?>">contact Us</a>
                     </div>
 
