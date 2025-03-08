75f181fce3c2cf901105862aff549c9c8d6adb84
TAMIL SELVAN M <tamilp435@gmail.com>
2025-03-08 15:25:17 +0530

    updated code

--- app/Controllers/Announcement.php
+++ app/Controllers/Announcement.php
@@ -8,19 +8,24 @@ use App\Models\Announcement as AnnouncementModel;
 class Announcement extends BaseController
 {
     public function index()
-    {
-        $bannerModel = new BannerModel();
-        $banner = $bannerModel->where('page', 'announcement')->where('is_published', 1)->first();
-        $announcementModel = new AnnouncementModel();
-        $announcements = $announcementModel->getAnnouncements();
+{
+    $bannerModel = new BannerModel();
+    $banner = $bannerModel->where('page', 'announcement')->where('is_published', 1)->first();
+    $announcementModel = new AnnouncementModel();
 
-        $data = [
-            'page_title' => 'Announcement',
-            'page_code' => 'announcement',
-            'banner' => $banner,
-            'announcements' => $announcements
-        ];
+    // Fetch announcements based on category
+    $school_updates = $announcementModel->getAnnouncements('school_updates');
+    $cbse_curriculum = $announcementModel->getAnnouncements('cbse_curriculum');
 
-        return view('header', $data) . view('announcement', $data) . view('footer');
-    }
+    $data = [
+        'page_title' => 'Announcement',
+        'page_code' => 'announcement',
+        'banner' => $banner,
+        'school_updates' => $school_updates, 
+        'cbse_curriculum' => $cbse_curriculum,
+        'announcements' => array_merge($school_updates, $cbse_curriculum) // Ensure this is passed
+    ];
+
+    return view('header', $data) . view('announcement', $data) . view('footer', $data);
+}
 }
